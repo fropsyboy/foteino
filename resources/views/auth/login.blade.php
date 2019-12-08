d2b0b7ff04cc4764916b5a0f01d4b3dbbd09c1e1
fropsyboy <sabidacit@gmail.com>
2019-12-08 09:59:31 +0100

    added the login module

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,81 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Foteino Portal :: Sign in </title>
+      <!-- Meta tags -->
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="keywords" content="Job search, company recruitment"
+         />
+      <script>
+         addEventListener("load", function () { setTimeout(hideURLbar, 0); }, false); function hideURLbar() { window.scrollTo(0, 1); }
+      </script>
+      <!-- Meta tags -->
+      <!--stylesheets-->
+      <link href="{{ asset('auth/css/style.css') }}" rel='stylesheet' type='text/css' media="all">
+      <!--//style sheet end here-->
+      <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+   </head>
+   <body>
+      <div class="mid-class">
+         <div class="art-right-w3ls">
+            <h2>Sign In to 
+                 <a href="https://foteinotalento.com" style="color: #205e7e">
+                    <font size="22"> <b> Foteino Talento </b></font>
+                </a>
+            </h2>
+            <form action="{{ route('login') }}" method="post">
+                            @csrf
+               <div class="main">
+                  <div class="form-left-to-w3l">
+                     <input type="text" name="email" placeholder="email" required autocomplete="email" autofocus>
+                     @error('email')
+                                  <span class="invalid-feedback" role="alert">
+                                        <strong style="color: #761b18;">{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                    @enderror
+                  </div>
+                  <div class="form-left-to-w3l ">
+                     <input type="password" name="password" placeholder="Password" required autocomplete="current-password">
+                     @error('password')
+                                  <span class="invalid-feedback" role="alert">
+                                        <strong style="color: #761b18;">{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    @enderror
+                     <div class="clear"></div>
+                  </div>
+               </div>
+               <div class="left-side-forget">
+                  <input type="checkbox" class="checked">
+                  <span class="remenber-me">Remember me </span>
+               </div>
+               <div class="right-side-forget">
+                  <a href="#" class="for">Forgot password...?</a>
+               </div>
+               <div class="clear"></div>
+               <div class="btnn">
+                  <button type="submit" style="background-color: #205e7e; border-radius: 12px;">Sign Up</button>
+               </div>
+            </form>
+            <div class="w3layouts_more-buttn">
+               <h3>Don't Have an account..?
+                  <a href="https://foteinotalento.com/sign-up">Sign Up Here
+                  </a>
+               </h3>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+ 
+         </div>
+         <div class="art-left-w3ls">
+            <h1 class="header-w3ls">
+            Welcome to Foteino Talento
+            </h1>
+         </div>
+      </div>
+      <footer class="bottem-wthree-footer">
+         <p>
+            © 2019 Foteino LTD All Rights Reserved | Design by
+            <a href="" target="_blank">Foteino</a>
+         </p>
+      </footer>
+   </body>
+</html>
